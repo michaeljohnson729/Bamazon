d58ac5262a6792df86e85d5197ec33b40f90b4ef
Michael Johnson <michaeljohnson@Michaels-MacBook-Pro.local>
2018-04-15 21:22:42 -0700

    added db connection and read table function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,31 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var cTable = require('console.table');
 
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "password",
+    database: "bamazon"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    printProducts();
+  });
+
+  function printProducts() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.table(res);
+        connection.end();
+      });
+  }
+  
